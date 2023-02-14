5c96a2196da9c09fe9713d2570ce1576e75928ab
theprajin <prazenthekiller@gmail.com>
2023-02-14 15:19:45 +0545

    initial commit

---
+++ lib/screens/simple_expenditure.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+class SimpleExpenditure extends StatefulWidget {
+  const SimpleExpenditure({super.key});
+
+  @override
+  State<SimpleExpenditure> createState() => _SimpleExpenditureState();
+}
+
+class _SimpleExpenditureState extends State<SimpleExpenditure> {
+  double? _netSalary;
+  double? _expenses;
+  String message = ' ';
+
+  final _form = GlobalKey<FormState>();
+
+  final _netSalaryFocusNode = FocusNode();
+  final _expensesFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _netSalaryFocusNode.dispose();
+    _expensesFocusNode.dispose();
+    super.dispose();
+  }
+
+  double calculations() {
+    return _netSalary! - _expenses!;
+  }
+
+  //It's called when the form is submitted using a button or through keyboard's "done" softkey
+  void _saveForm() {
+    _form.currentState?.validate();
+    _form.currentState?.save();
+
+    setState(() {
+      message = ' ';
+      if (_netSalary != null && _expenses != null) {
+        if (_netSalary! > _expenses!) {
+          message = 'You have gained ${calculations()} this month';
+        } else if (_netSalary! < _expenses!) {
+          message = 'You have lost ${calculations()} this month';
+        } else {
+          message = 'Your balance hasn\'t changed';
+        }
+
+        calculations();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Simple Expenditure'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _form,
+          child: ListView(
+            children: [
+              Center(
+                child: Text(message),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 5.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Net Salary',
+                          ),
+                          textInputAction: TextInputAction.next,
+                          keyboardType: TextInputType.number,
+                          focusNode: _netSalaryFocusNode,
+                          onFieldSubmitted: (_) {
+                            FocusScope.of(context)
+                                .requestFocus(_expensesFocusNode);
+                          },
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter the value';
+                            }
+                            if (double.tryParse(value) == null) {
+                              return 'Please enter a valid number';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            if (value!.isNotEmpty) {
+                              _netSalary = double.parse(value);
+                            } else {
+                              _netSalary = null;
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 5.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Expenses',
+                          ),
+                          textInputAction: TextInputAction.done,
+                          keyboardType: TextInputType.number,
+                          focusNode: _expensesFocusNode,
+                          onFieldSubmitted: (_) {
+                            _saveForm();
+                          },
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter the value';
+                            }
+                            if (double.tryParse(value) == null) {
+                              return 'Please enter a valid number';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            if (value!.isNotEmpty) {
+                              _expenses = double.parse(value);
+                            } else {
+                              _expenses = null;
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _saveForm();
+                },
+                child: const Text('Calculate'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
